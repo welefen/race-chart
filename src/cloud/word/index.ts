4f676c0e1395292944b565349af43e7cf8d236c2
welefen <welefen@gmail.com>
2020-05-18 18:12:13 +0800

    update

--- src/cloud/word/index.ts
+++ src/cloud/word/index.ts
@@ -6,7 +6,7 @@ import { Position } from '../../common/types';
 export class WordCloud extends Cloud {
   private async putWord(str: string, weight?: number, deg: number = 0) {
     deg = deg || this.getRotateDeg();
-    const info = await this.getTextInfo(str, weight, 90);
+    const info = await this.getTextInfo(str, weight, 45);
     let r = this.maxRadius + 1;
     while (r--) {
       const points = this.getPointsAtRadius(r, this.center);
@@ -25,21 +25,36 @@ export class WordCloud extends Cloud {
     const styles = { ...textStyle, fontSize, rotate: deg };
     const label = createLabel(str, styles);
     this.layer.appendChild(label);
-    label.attr({ fillColor: 'red', opacity: 0 });
+    label.attr({ fillColor: 'red', anchor: [0.5, 0.5], padding: [gridSize, gridSize, gridSize, gridSize] });
     await label.textImageReady;
+    const cos = Math.cos(deg * Math.PI / 180);
+    const sin = Math.sin(deg * Math.PI / 180);
     const { image } = label.textImage;
+    const borderSize = label.borderSize;
+    const hw = borderSize[0] / 2;
+    const hh = borderSize[1] / 2;
+    label.attr({
+      x: Math.sqrt((hw * cos) ** 2 + (hh * sin) ** 2) + gridSize,
+      y: Math.sqrt((hw * sin) ** 2 + (hh * cos) ** 2) + gridSize,
+    });
+
+
+    const dpr = this.config.displayRatio
     let { width, height } = label.getBoundingClientRect();
     const gridWidth = Math.ceil(width / gridSize);
     const gridHeight = Math.ceil(height / gridSize);
     width = gridWidth * gridSize;
     height = gridHeight * gridSize;
-    const canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    const ctx = canvas.getContext('2d');
-    const dpr = this.config.displayRatio;
-    ctx.scale(dpr, dpr);
+
+    const cc = document.createElement('canvas')
+    cc.width = width * dpr;
+    cc.height = height * dpr;
+    cc.style.width = `${width}px`;
+    cc.style.height = `${height}px`;
+
+    const ctx = cc.getContext('2d');
     const m = label.renderMatrix;
+    ctx.scale(dpr, dpr);
     ctx.transform(m[0], m[1], m[2], m[3], m[4], m[5]);
     const w = image.width / dpr;
     const h = image.height / dpr;
@@ -47,8 +62,9 @@ export class WordCloud extends Cloud {
     const x = -anchor[0] * w;
     const y = -anchor[1] * h;
     ctx.drawImage(image, x, y, w, h);
-    document.body.appendChild(canvas);
-    const imageData = ctx.getImageData(0, 0, width, height);
+    document.body.appendChild(cc);
+
+    const imageData = ctx.getImageData(0, 0, width * dpr, height * dpr);
     const occupied = this.getImageOccupied(imageData);
     return {
       node: label,
